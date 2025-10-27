157b6e9a6e3f192b434395ae3f5f46dc529dc6f1
GorkovAnatolii <96316257+GorkovAnatolii@users.noreply.github.com>
2025-10-28 00:19:08 +0300

    Add two level generation methods: random and from file

--- src/main/java/ru/mipt/bit/platformer/Tank.java
+++ src/main/java/ru/mipt/bit/platformer/Tank.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.math.GridPoint2;
 public class Tank extends GameObject {
     private GridPoint2 destinationCoordinates;
     private float movementProgress = 1f;
+    private static final float MOVEMENT_SPEED = 2f;
 
     public Tank(TextureRegion graphics, GridPoint2 coordinates) {
         super(graphics, coordinates, 0f);
@@ -16,16 +17,8 @@ public class Tank extends GameObject {
         return movementProgress;
     }
 
-    public void setMovementProgress(float movementProgress) {
-        this.movementProgress = movementProgress;
-    }
-
     public GridPoint2 getDestinationCoordinates() {
         return destinationCoordinates;
-    }
-
-    public void setDestinationCoordinates(GridPoint2 destinationCoordinates) {
-        this.destinationCoordinates = destinationCoordinates;
     }
 
     public void move(Direction direction) {
@@ -38,6 +31,33 @@ public class Tank extends GameObject {
     }
 
     public boolean canMove() {
-        return movementProgress == 1f;
+        return movementProgress >= 1f;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        if (movementProgress < 1f) {
+            movementProgress += MOVEMENT_SPEED * deltaTime;
+            if (movementProgress > 1f) {
+                movementProgress = 1f;
+                coordinates.set(destinationCoordinates);
+            }
+            updateBoundsPosition();
+        }
+    }
+
+    public Tank(GridPoint2 coordinates) {
+    super(coordinates, 0f);
+    this.destinationCoordinates = new GridPoint2(coordinates);
+    }
+
+
+    public void update(float deltaTime) {
+    if (movementProgress < 1f) {
+        movementProgress = Math.min(1f, movementProgress + deltaTime * 2f);
+        if (movementProgress >= 1f) {
+            coordinates.set(destinationCoordinates);
+        }
+      }
     }
 }
